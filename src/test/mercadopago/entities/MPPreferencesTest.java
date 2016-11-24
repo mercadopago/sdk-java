58aa1182c2ecfe3c687014afe7c51bec70077363
Eduardo Paoletta <eduardo.paoletta@mercadolibre.com>
2016-11-24 15:23:44 -0300

    Resources: Preference tests

---
+++ src/test/mercadopago/entities/MPPreferencesTest.java
@@ -0,0 +1,61 @@
+package test.mercadopago.entities;
+
+import com.mercadopago.core.MPValidator;
+import org.junit.Test;
+
+/**
+ * Mercado Pago SDK
+ *
+ *
+ * Created by Eduardo Paoletta on 11/21/16.
+ */
+public class MPPreferencesTest {
+
+    @Test
+    public void preferenceTest() {
+/*
+        Shipments shipments = new Shipments();
+        shipments
+                .setMode(Shipments.ShipmentMode.not_specified);
+
+        HashMap<String, Object> auxiliar_data = new HashMap<String, Object>();
+        auxiliar_data.put("aux_string", "aaaa");
+        auxiliar_data.put("aux_int", 123);
+        auxiliar_data.put("aux_bool", true);
+        auxiliar_data.put("aux_date", new Date());
+        Object obj = new Object();
+        auxiliar_data.put("aux_obj", obj);
+
+        Preferences preferences = new Preferences();
+        preferences
+                //Size
+                .setId("Some Id")
+                //Date
+                .setDateCreated(new Date())
+                //Boolean
+                .setExpires(Boolean.TRUE)
+                //Integer
+                .setClientId(666)
+                //Float
+                .setMarketplaceFee(1.12f)
+                //Object --> enum
+                .setShipments(shipments)
+                .auxiliar_data = auxiliar_data;
+
+
+        try {
+            MPValidator.validate(preferences);
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+
+        System.out.print("Shipment mode: ");
+        System.out.println(preferences.getShipments().getMode());
+
+        JsonObject json = preferences.getJson();
+
+        System.out.println("Json: ");
+        System.out.println(json.toString());
+*/
+    }
+}
