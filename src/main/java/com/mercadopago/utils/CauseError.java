e538f14310fd592e376107a92b1d12e2e155b5fc
Jorge Gonzalez <ljorgelgonzalez@gmail.com>
2018-12-07 21:27:41 -0430

    Agregado 2 utilidades para la respuesta en caso de error

---
+++ src/main/java/com/mercadopago/utils/CauseError.java
@@ -0,0 +1,36 @@
+package com.mercadopago.utils;
+
+public class CauseError {
+
+	private String description;
+	private String code;
+
+	/**
+	 * @return el description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description el description a establecer
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return el code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code el code a establecer
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
---
+++ src/main/java/com/mercadopago/utils/ResponseError.java
@@ -0,0 +1,68 @@
+package com.mercadopago.utils;
+
+import java.util.List;
+
+public class ResponseError {
+
+	private String message;
+	private Integer status;
+	private String error;
+	private List<CauseError> cause;
+
+	/**
+	 * @return el message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message el message a establecer
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * @return el status
+	 */
+	public Integer getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status el status a establecer
+	 */
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return el error
+	 */
+	public String getError() {
+		return error;
+	}
+
+	/**
+	 * @param error el error a establecer
+	 */
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	/**
+	 * @return el cause
+	 */
+	public List<CauseError> getCause() {
+		return cause;
+	}
+
+	/**
+	 * @param cause el cause a establecer
+	 */
+	public void setCause(List<CauseError> cause) {
+		this.cause = cause;
+	}
+
+}
