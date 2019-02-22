5e3856c95442410fff22fbccc5ee5517ee0652bb
Pablo Abal <pablo.abal@mercadolibre.com>
2019-02-22 17:23:04 -0300

    including content

--- src/main/java/com/mercadopago/resources/datastructures/chargeback/Documentation.java
+++ src/main/java/com/mercadopago/resources/datastructures/chargeback/Documentation.java
@@ -1,4 +1,7 @@
 package com.mercadopago.resources.datastructures.chargeback;
 
 public class Documentation {
+    private String type = null;
+    private String url = null;
+    private String description = null;
 }
