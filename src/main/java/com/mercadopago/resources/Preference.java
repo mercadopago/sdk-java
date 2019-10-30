00b606aaec138e38164dad712d7ac209b38614e4
Javier Ezequiel Carugati <javier.carugati@mercadolibre.com>
2019-10-30 09:26:42 -0300

    added setter for OperationType and new operationType

--- src/main/java/com/mercadopago/resources/Preference.java
+++ src/main/java/com/mercadopago/resources/Preference.java
@@ -45,7 +45,8 @@ public class Preference extends MPBase {
 
     public enum OperationType {
         regular_payment,
-        money_transfer
+        money_transfer,
+        pos_payment
     }
     @Size(max=600) private String additionalInfo = null;
     private AutoReturn autoReturn = null;
@@ -151,6 +152,11 @@ public class Preference extends MPBase {
 
     public OperationType getOperationType() {
         return operationType;
+    }
+
+    public Preference setOperationType(OperationType operationType){
+        this.operationType = operationType;
+        return this;
     }
 
     public String getAdditionalInfo() {
