40f4a470b6237c51f9b1872b251144038c027110
micheloliveiramarciano <michel.marciano@mercadolivre.com>
2020-06-17 16:36:09 -0300

    testes unitarios

--- src/test/java/mercadopago/resources/PreferenceTest.java
+++ src/test/java/mercadopago/resources/PreferenceTest.java
@@ -30,7 +30,7 @@ public class PreferenceTest {
     public static void beforeTest() throws MPException {
         MercadoPago.SDK.cleanConfiguration();
       //  MercadoPago.SDK.setAccessToken(System.getenv("ACCESS_TOKEN_TEST"));
-      MercadoPago.SDK.setAccessToken("TEST-1171843254961250-060519-6945ff28b0cd07595e34c562537286ea-571119597");
+      MercadoPago.SDK.setAccessToken("${token}");
 
     }
 
