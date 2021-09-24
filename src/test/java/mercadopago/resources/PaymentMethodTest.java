e278c7c43249658e2f4f38be1b58baa2bc02f497
rhames07 <rhames.lima@mercadolivre.com>
2021-09-24 14:56:03 -0300

    Improved Payment Methods test

--- src/test/java/mercadopago/resources/PaymentMethodTest.java
+++ src/test/java/mercadopago/resources/PaymentMethodTest.java
@@ -9,6 +9,7 @@ import com.mercadopago.core.MPResourceArray;
 import com.mercadopago.exceptions.MPException;
 import com.mercadopago.resources.PaymentMethod;
 import java.io.IOException;
+import java.util.List;
 import org.junit.Test;
 
 public class PaymentMethodTest extends BaseResourceTest {
@@ -25,5 +26,21 @@ public class PaymentMethodTest extends BaseResourceTest {
     assertEquals(HTTP_STATUS_OK, response.getLastApiResponse().getStatusCode());
     assertNotNull(response.resources());
     assertTrue(response.resources().size() > 0);
+    for (PaymentMethod paymentMethod : (List<PaymentMethod>) response.resources()) {
+      assertNotNull(paymentMethod.getId());
+      assertNotNull(paymentMethod.getName());
+      assertNotNull(paymentMethod.getPaymentTypeId());
+      assertNotNull(paymentMethod.getAccreditationTime());
+      assertNotNull(paymentMethod.getAdditionalInfoNeeded());
+      assertNotNull(paymentMethod.getDeferredCapture());
+      assertNotNull(paymentMethod.getFinancialInstitutions());
+      assertNotNull(paymentMethod.getMaxAllowedAmount());
+      assertNotNull(paymentMethod.getMinAllowedAmount());
+      assertNotNull(paymentMethod.getProcessingModes());
+      assertNotNull(paymentMethod.getStatus());
+      assertNotNull(paymentMethod.getSecureThumbnail());
+      assertNotNull(paymentMethod.getThumbnail());
+      assertNotNull(paymentMethod.getSettings());
+    }
   }
 }
