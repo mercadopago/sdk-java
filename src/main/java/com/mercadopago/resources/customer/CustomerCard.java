bb4de0abb8ec46cd45f8a35172980d98860cf401
Romero Silva <romero.silva@mercadolivre.com>
2022-01-18 18:11:42 -0300

    Add liveMode, userId to CustomerCard
    also add javadoc

--- src/main/java/com/mercadopago/resources/customer/CustomerCard.java
+++ src/main/java/com/mercadopago/resources/customer/CustomerCard.java
@@ -9,39 +9,45 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class CustomerCard extends MPResource {
-  /** */
+  /** Id of the card. */
   private String id;
 
-  /** */
+  /** Id of the customer. */
   private String customerId;
 
-  /** */
+  /** Month the card expires. */
   private Integer expirationMonth;
 
-  /** */
+  /** Year the card expires. */
   private Integer expirationYear;
 
-  /** */
+  /** First six digits of the card. */
   private String firstSixDigits;
 
-  /** */
+  /** Last four digits of the card. */
   private String lastFourDigits;
 
-  /** */
+  /** Data related to the chosen payment method. */
   private CustomerCardPaymentMethod paymentMethod;
 
-  /** */
+  /** Security code of the card. */
   private CustomerCardSecurityCode securityCode;
 
-  /** */
+  /** Card issuer. */
   private CustomerCardIssuer issuer;
 
-  /** */
+  /** Data related to the holder of the card, usually the customer. */
   private CustomerCardCardholder cardholder;
 
-  /** */
+  /** Creation date of the record. */
   private Date dateCreated;
 
-  /** */
+  /** Date the record was last updated. */
   private Date dateLastUpdated;
+
+  /** Id of the user. */
+  private String userId;
+
+  /** Flag indicating if this is a record from production or test environment. */
+  private boolean liveMode;
 }
