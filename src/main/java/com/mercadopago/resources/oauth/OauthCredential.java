b17b99be31f2c9262bbe983f045a599d1edf747e
Romero Silva <romero.silva@mercadolivre.com>
2022-01-31 09:51:45 -0300

    Add javadoc

--- src/main/java/com/mercadopago/resources/oauth/OauthCredential.java
+++ src/main/java/com/mercadopago/resources/oauth/OauthCredential.java
@@ -3,17 +3,21 @@ package com.mercadopago.resources.oauth;
 import com.mercadopago.net.MPResource;
 import lombok.Getter;
 
-/** */
+/** Credential information for an Oauth authorization. */
 @Getter
 public class OauthCredential extends MPResource {
-  /** */
+  /** Access token of the user */
   private String accessToken;
-  /** */
+
+  /** Type of token */
   private String tokenType;
-  /** */
+
+  /** Expiration date of the token */
   private Long expiresIn;
-  /** */
+
+  /** The Oauth scope associated to the token */
   private String scope;
-  /** */
+
+  /** Token used to renew Oauth credential */
   private String refreshToken;
 }
