dfa99d5d35c768b91ef3f03afd862bdc0b9c6173
Romero Silva <romero.silva@mercadolivre.com>
2022-02-07 18:40:25 -0300

    Add content type json, default timeout
    to BaseClientTest

--- src/test/java/mercadopago/BaseClientTest.java
+++ src/test/java/mercadopago/BaseClientTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.BeforeAll;
 
 /** BaseClientTest class. */
 public abstract class BaseClientTest {
+  protected static final String CONTENT_TYPE_APPLICATION_JSON = "application/json";
+
+  protected static final int DEFAULT_TIMEOUT = 1000;
+
   public static HttpClientMock httpClientMock = new HttpClientMock();
 
   public static HttpClient httpClient = httpClientMock.getHttpClient();
