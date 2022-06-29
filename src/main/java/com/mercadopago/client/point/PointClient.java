4096fd843f3da74a53337b0a842e13253c6cb4a6
rhames07 <rhames.lima@mercadolivre.com>
2022-06-29 12:31:22 -0300

    Adjust javadoc

--- src/main/java/com/mercadopago/client/point/PointClient.java
+++ src/main/java/com/mercadopago/client/point/PointClient.java
@@ -154,7 +154,7 @@ public class PointClient extends MercadoPagoClient {
    * @return cancelled payment intent id
    * @throws MPException an error if the request fails
    * @see <a
-   *     href="https://www.mercadopago.com.br/developers/pt/reference/integrations_api/_point_integration-api_devices_deviceid_payment-intents_paymentintentid/delete">api
+   *     href="https://www.mercadopago.com/developers/en/reference/integrations_api/_point_integration-api_devices_deviceid_payment-intents_paymentintentid/delete">api
    *     docs</a>
    */
   public PointCancelPaymentIntent cancelPaymentIntent(String deviceId, String paymentIntentId)
