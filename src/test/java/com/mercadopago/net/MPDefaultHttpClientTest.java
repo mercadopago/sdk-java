657345a1c10092c61e6750778420b1d91afb99f4
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2023-08-23 18:59:22 -0300

    Add fix for response entity null

--- src/test/java/com/mercadopago/net/MPDefaultHttpClientTest.java
+++ src/test/java/com/mercadopago/net/MPDefaultHttpClientTest.java
@@ -1,11 +1,14 @@
 package com.mercadopago.net;
 
 import static com.mercadopago.helper.MockHelper.generateHttpResponseFromFile;
+import static com.mercadopago.net.HttpStatus.INTERNAL_SERVER_ERROR;
 import static com.mercadopago.net.HttpStatus.OK;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 import com.mercadopago.BaseClientTest;
 import com.mercadopago.exceptions.MPApiException;
@@ -77,5 +80,29 @@ class MPDefaultHttpClientTest extends BaseClientTest {
     assertNotNull(response);
     assertEquals(OK, response.getStatusCode());
     assertEquals("{\n" + "  \"success\": true\n" + "}", response.getContent());
+  }
+
+  @Test
+  void sendWithIOException() throws MPException, IOException {
+    mpDefaultHttpClient = new MPDefaultHttpClient(HTTP_CLIENT);
+
+    Map<String, String> headers = new HashMap<>();
+    headers.put("x-test", "test");
+
+    MPRequest request =
+            MPRequest.builder().method(HttpMethod.GET).uri("http://test.com").headers(headers).build();
+
+    HttpResponse httpResponse = generateHttpResponseFromFile(responseGenericSuccessJson, OK);
+    httpResponse.setHeader("x-test", "test");
+    doThrow(IOException.class)
+            .when(HTTP_CLIENT)
+            .execute(any(HttpRequestBase.class), any(HttpContext.class));
+
+    try {
+      mpDefaultHttpClient.send(request);
+    } catch (MPApiException ex) {
+      assertEquals(INTERNAL_SERVER_ERROR, ex.getStatusCode());
+      assertEquals("", ex.getApiResponse().getContent());
+    }
   }
 }
