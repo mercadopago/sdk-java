c55f0fa43415766c1c77c5b15931286617d818d3
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-02-23 17:06:06 -0300

    Remove builder from response class

--- src/main/java/com/mercadopago/resources/payment/PaymentBarcode.java
+++ src/main/java/com/mercadopago/resources/payment/PaymentBarcode.java
@@ -1,11 +1,9 @@
 package com.mercadopago.resources.payment;
 
-import lombok.Builder;
 import lombok.Getter;
 
 /** PaymentBarcode class. */
 @Getter
-@Builder
 public class PaymentBarcode {
     /** Barcode content. */
     private String content;
