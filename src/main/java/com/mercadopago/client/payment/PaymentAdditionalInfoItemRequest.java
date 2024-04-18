3f35969b2e13a3f76b8f1f433b6aa13adaa0b601
VickieAranha <vickie.costa@mercadolivre.com>
2024-04-18 12:23:04 -0300

    ajust event date payment

--- src/main/java/com/mercadopago/client/payment/PaymentAdditionalInfoItemRequest.java
+++ src/main/java/com/mercadopago/client/payment/PaymentAdditionalInfoItemRequest.java
@@ -1,18 +1,2 @@
-package com.mercadopago.client.payment;
-
-
-import java.time.OffsetDateTime;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class PaymentAdditionalInfoItemRequest extends PaymentItemRequest {
-
-  /**
-   * Date of event
-   */
-  private OffsetDateTime eventDate;
-}
 
 
--- src/main/java/com/mercadopago/resources/payment/PaymentAdditionalInfoItem.java
+++ src/main/java/com/mercadopago/resources/payment/PaymentAdditionalInfoItem.java
@@ -2,10 +2,12 @@ package com.mercadopago.resources.payment;
 
 import com.mercadopago.resources.common.Address;
 import java.time.OffsetDateTime;
+import lombok.Builder;
 import lombok.Getter;
 
 /** PaymentAdditionalInfoIten class. */
 @Getter
+@Builder
 public class PaymentAdditionalInfoItem extends PaymentItem {
 
 
