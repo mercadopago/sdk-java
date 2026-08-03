e938d3f88838cabc7d3611b08f2b8fc2e895bd96
Diego Barajas <diego.barajassuarez@mercadolibre.com.co>
2026-08-03 18:24:22 -0500

    fix: add missing semicolon to ArgumentMatchers import in MPAutoPagingIterableTest

--- src/test/java/com/mercadopago/net/MPAutoPagingIterableTest.java
+++ src/test/java/com/mercadopago/net/MPAutoPagingIterableTest.java
@@ -2,7 +2,7 @@ package com.mercadopago.net;
 
 import static com.mercadopago.helper.MockHelper.generateHttpResponseFromFile;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import com.mercadopago.BaseClientTest;
